b7ceaaff7ed010895ec21d8da6ae85e6b8531a41
Ryan Walmsley <ryanteck@gmail.com>
2016-11-14 11:50:59 +0000

    Added in some Piglow tests for I2C Development and the files for smbus

---
+++ RTk/smbus.py
@@ -0,0 +1,37 @@
+#RTK.GPIO implementation of SMBUS
+import rtkserial
+import pprint
+from time import sleep
+serial = rtkserial.s
+print("imported rtkbus")
+i = 0.1
+class SMBus:
+
+    def __init__(self,bus):
+        self.bus = bus
+    def _write(self,str):
+        #print((str))
+        serial.write(str)
+
+    def write_i2c_block_data(self,i2caddress,command,data):
+
+        #print(i2caddress)
+        #print(command)
+        #print(data)
+        #Get the address and convert it to 8 bit for mbed
+        i2caddress = hex(i2caddress<<1)
+        #and now convert that to a letter
+        i2caddrchar = chr(int(i2caddress,0))
+        self._write("IW") #I2C write
+        sleep(i)
+        self._write(chr(int(len(data))))
+        sleep(i)
+        self._write(i2caddrchar) # Write the 8 Bit I2C Address
+        sleep(i)
+        self._write(chr(int(hex(command),0))) # Write the command char
+        sleep(i)
+        #Now write address to serial port
+        for dataVal in data: #Write each item of data
+            self._write(chr(int(hex(dataVal),0))) #Data
+            sleep(i)
+        print("wrote block")
---
+++ i2cwritetest.py
@@ -0,0 +1,7 @@
+import time
+
+import piglow
+#import sleep
+
+piglow.red(64)
+piglow.show()
