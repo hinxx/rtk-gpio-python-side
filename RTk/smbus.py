e0aa26e1e87968d8ed2943975f7f41ffaf086415
Ryan Walmsley <ryanteck@gmail.com>
2016-12-08 09:31:04 +0000

    Tidy up i2cwrite a little

--- RTk/smbus.py
+++ RTk/smbus.py
@@ -15,28 +15,17 @@ class SMBus:
 
     def write_i2c_block_data(self,i2caddress,command,data):
 
-        #pprint.pprint(str(i2caddress)+","+str(command)+","+str(data))
-        #Get the address and convert it to 8 bit for mbed
-        i2caddress = hex(i2caddress<<1)
+        #Get the address and convert it to 8 bit for mbed and then convert to the char to send over.
+        i2caddrchar = chr(int(hex(i2caddress<<1),0))
         #and now convert that to a letter
-        i2caddrchar = chr(int(i2caddress,0))
         self._write("IW") #I2C write
-        #sleep(i)
-        self._write(i2caddrchar) # Write the 8 Bit I2C Address
-        #sleep(i)
+        self._write(i2caddrchar) # Write the 8 Bit I2C address
         self._write(chr(int(len(data))))
-
-        #sleep(i)
         self._write(chr(int(hex(command),0))) # Write the command char
-        #sleep(i)
-        #Now write address to serial port
         for idx, dataVal in enumerate(data): #Write each item of data
-            #print(chr(int(hex(dataVal),0)))
             self._write(chr(int(hex(dataVal),0))) #Data
             if(idx != len(data)):
             	sleep(i)
-        #print("wrote block")
-        #raw_input()
 
     def read_word_data(self,i2caddress,command) :
         i2caddress = hex(i2caddress<<1)
